db96780927946a6011c7ee10929853f95778ae5f
Yuliia Diachenko <zirkanew82@gmail.com>
2024-12-19 13:18:27 +0200

    add mobile menu

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,10 @@
+//we make the color of the link in the active state the corresponding color
 document.addEventListener('DOMContentLoaded', event => {
   const links = document.querySelectorAll('.header-nav-link');
 
   links.forEach(link => {
     link.addEventListener('click', function (event) {
-      event.preventDefault(); // запобігаємо переходу по посиланню
+      event.preventDefault(); // we prevent going through the link
       links.forEach(link => link.classList.remove('active'));
       link.classList.add('active');
     });
@@ -12,6 +13,29 @@ document.addEventListener('DOMContentLoaded', event => {
   document.addEventListener('click', function (event) {
     if (!event.target.classList.contains('header-nav-link')) {
       links.forEach(link => link.classList.remove('active'));
+    }
+  });
+});
+// mobile menu
+document.addEventListener('DOMContentLoaded', event => {
+  const burgerMenu = document.querySelector('.mobile-menu-open-btn');
+  const mobileMenu = document.querySelector('.mobile-menu-wraper');
+  const closeMenuBtn = document.querySelector('.mobile-menu-wraper-btn-close');
+
+  // Відкриття мобільного меню
+  burgerMenu.addEventListener('click', () => {
+    mobileMenu.classList.add('is-open');
+  });
+
+  // Закриття мобільного меню
+  closeMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('is-open');
+  });
+
+  // Закриття мобільного меню при натисканні клавіші Esc
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      mobileMenu.classList.remove('is-open');
     }
   });
 });
